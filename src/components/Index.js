7e16e328f592a6204a435a837d45e9337d8804b7
Brandon <brandon.lau86@gmail.com>
2019-10-29 23:17:41 -0400

    Clean up code and add comments

--- src/components/Index.js
+++ src/components/Index.js
@@ -9,62 +9,67 @@ export default class Index extends React.Component
 			allPics: []
 	}
 	handleSearch = (value) => {				//Sets state, then calls submit search (this fn is passed to children)
-	    this.setState({
-	        allPics: [],                            //This step is necessary to stop the animation from stuttering from existing pics to new pics
-	        searchFor: value
-	    }, () => {
-	        this.submitSearch();
-	    });
+	  this.setState({
+	    allPics: [],                            //This step is necessary to stop the animation from stuttering from existing pics to new pics
+	    searchFor: value
+	  }, () => {
+	    this.submitSearch();
+	  });
+  }
+  
+	//fetches to Rails backend
+  submitSearch = () => {																					
+  	if (this.state.searchFor === "random")
+  	{
+  	  fetch(`https://wonderpic-backend.herokuapp.com/random`)
+  	  .then(resp => resp.json())
+  	  .then(data => {
+  	    this.setState({
+  	        allPics: data
+  	    })
+  	  })
+  	}
+  	else {
+  		fetch(`https://wonderpic-backend.herokuapp.com/search`,
+  		{
+  		  method: "POST",
+  		  headers: {
+  		    "Content-Type": "application/json"
+  		  },
+  		  body: JSON.stringify({
+  		  	"Search": `${this.state.searchFor}`
+  		  })
+  		})
+  		.then(resp => resp.json())
+  		.then((data) => {
+  		  this.setState({
+  		    allPics: data.results
+  		  })
+  		})
+  	}
 	}
-  submitSearch = () => {																					//fetches to Rails backend 
-      if (this.state.searchFor === "random") 
-      {
-        fetch(`https://wonderpic-backend.herokuapp.com/random`)
-        .then(resp => resp.json())
-        .then(data => {
-          this.setState({
-              allPics: data
-          })
-        })
-      }
-      else {
-      	fetch(`https://wonderpic-backend.herokuapp.com/search`, 
-      	{
-      	  method: "POST",
-      	  headers: {
-      	      "Content-Type": "application/json"
-      	  },
-      	  body: JSON.stringify({
-      	      "Search": `${this.state.searchFor}`
-      	  })
-      	})
-      	.then(resp => resp.json())
-      	.then((data) => {
-      	  this.setState({
-      	    allPics: data.results
-      	  })
-      	})
-      }
-	}
-  mapPics = () => {																					//Produces all the individual pics 
+	
+  mapPics = () => {																					//Produces all the individual pics
     let mapped = this.state.allPics.map((pic) => {
-        return <IndividualPic key = {this.restartAnimation()} search = {this.handleSearch} pic = {pic}/>
+      return <IndividualPic key = {this.restartAnimation()} search = {this.handleSearch} pic = {pic}/>
     })
     return mapped;
 	}
-	restartAnimation = () => {																//Necessary to make sure animations happen every time search happens, rather than only first time 
+
+	restartAnimation = () => {																//Necessary to make sure animations happen every time search happens, rather than only first time
 		let random = Math.floor(Math.random()*900);
 		return random;
 	}
-  render() 
+	
+  render()
   {
     return (
     	<div >
       	<TitleArea handleSearch = {this.handleSearch}/>
 				<h1 key = {Math.floor(Math.random()*300)} className = "searchFor" >{this.state.searchFor ? this.state.searchFor : ""}</h1>
-        	<div className = "mainDiv">
-          	{this.mapPics()}
-          </div>
+        <div className = "mainDiv">
+          {this.mapPics()}
+        </div>
 			</div>
     )
   }
