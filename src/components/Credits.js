661d91b73522d1ac79e6d4a576943cc36fa4388f
Brandon <brandon.lau86@gmail.com>
2019-10-31 15:12:30 -0400

    Lowercase photos by in Credits

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -3,8 +3,7 @@ import React from "react"
 
 export default function Credits(props) 
 {
-
-	return(
-		<p className = "credits">Photo by {props.fullName} on Unsplash</p>
+	return (
+		<p className = "credits">photo by {props.fullName} on Unsplash</p>
 	)
 }
